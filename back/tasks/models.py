b766dec821794d848023a43d0c838cba0eba4daa
Fouad HACHOUR <hachour5fouad@gmail.com>
2024-12-14 22:18:37 +0100

    wip

--- back/tasks/models.py
+++ back/tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    comlexity = models.IntegerField(range(1, 10))
+    duration = models.IntegerField(range(1, 60*8))
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
